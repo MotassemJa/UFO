bb2ee98821c26e0ffda3e304ef3d1d1a6b3a8381
MotassemJa <M.j.92@live.com>
2018-11-09 07:57:52 +0100

    Added BottomNavogationView

---
+++ app/src/main/java/com/githun/motassemja/ufo/ui/fragments/DashboardFragment.java
@@ -0,0 +1,14 @@
+package com.githun.motassemja.ufo.ui.fragments;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * Copyright (c) 2018 T-Systems Multimedia Solutions GmbH
+ * Riesaer Str. 5, D-01129 Dresden, Germany
+ * All rights reserved.
+ * Name: DashboardFragment
+ * Autor: moja
+ * Datum: 08.11.2018
+ */
+public class DashboardFragment extends Fragment {
+}
